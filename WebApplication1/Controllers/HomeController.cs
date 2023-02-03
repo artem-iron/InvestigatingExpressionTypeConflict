b37265f392a79074d9e9270d21b1a8e0a882aa51
Artem Koloskov <artem@ironsoftware.com>
2023-02-03 17:07:53 +0700

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IronXL.WorkSheet ws = new IronXL.WorkBook().DefaultWorkSheet;
+
+            ws["A2"].StringValue = "ass";
+
+            System.Func<IronXL.Cell, bool> ass = (cell) =>
+            {
+                if (cell.StringValue == "ass")
+                {
+                    return true;
+                }
+                else
+                    return false;
+            };
+
+            IronXL.Cell asss = ws["A1:A2"].FirstOrDefault(ass);
+
+            System.Linq.Expressions.ExpressionType expressionType = System.Linq.Expressions.ExpressionType.Add;
+
+            ViewBag.Title = expressionType.ToString() + asss.StringValue + ass.ToString();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
